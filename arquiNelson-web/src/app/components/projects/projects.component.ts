c22db7a9628edabf72971b9db954b25507cd55e8
Palometry <pdazatello@gmail.com>
2026-01-20 17:12:03 -0500

    ajustes a los overlay portafolio

--- arquiNelson-web/src/app/components/projects/projects.component.ts
+++ arquiNelson-web/src/app/components/projects/projects.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { AfterViewInit, ChangeDetectorRef, Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { PortfolioListItem, PortfolioService } from '../../services/portfolio';
 
@@ -10,15 +10,36 @@ import { PortfolioListItem, PortfolioService } from '../../services/portfolio';
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent implements OnInit, AfterViewInit {
   items: PortfolioListItem[] = [];
   loading = false;
+  private loaded = false;
+  private readonly isBrowser: boolean;
 
-  constructor(private portfolioService: PortfolioService) {}
+  constructor(
+    private portfolioService: PortfolioService,
+    private cdr: ChangeDetectorRef,
+    @Inject(PLATFORM_ID) platformId: object
+  ) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
 
   async ngOnInit() {
+    this.tryLoad();
+  }
+
+  async ngAfterViewInit() {
+    this.tryLoad();
+  }
+
+  private async tryLoad() {
+    if (!this.isBrowser || this.loaded) {
+      return;
+    }
+    this.loaded = true;
     this.loading = true;
     this.items = await this.portfolioService.getItems();
     this.loading = false;
+    this.cdr.detectChanges();
   }
 }
